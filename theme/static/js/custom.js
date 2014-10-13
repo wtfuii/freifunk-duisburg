1d89a9c8f296e38695f8aaa0c05f1414b453e8a1
wtfuii <KlausD@NB000249.ads.abit.de>
2014-10-13 17:21:43 +0200

    did several stuff including javascript

--- theme/static/js/custom.js
+++ theme/static/js/custom.js
@@ -1,4 +1,43 @@
-$(document).ready(
-    $('[data-toggle="popover"]').popover({placement: "bottom", html: "true", title: "Suche"}),
-    $('.test').each(function(){console.log(this)})
+$(document).ready(function () {
+        //popover for search-icon:
+        $('[data-toggle="popover"]').popover({placement: "bottom", html: "true", title: "Suche"})
+
+        //count of active nodes and clients for display in footer:
+        var mapcount = function () {
+            $.getJSON("/static/nodes.json", function (data) {
+                var nodecount = 0;
+                var clientcount = 0;
+                $.each(data.nodes, function () {
+                    if (this.firmware != null) {
+                        nodecount += 1;
+                    }
+                })
+                $.each(data.links, function () {
+                    if (this.type == "client") {
+                        clientcount += 1;
+                    }
+                })
+                console.log(nodecount);
+                console.log(clientcount)
+                $(".mapcount").append('<li>Knoten online: ' + nodecount + '</li><li>Clients online: ' + clientcount + '</li>')
+            })
+        }
+        mapcount();
+
+        //style Forms:
+        $("input[type!=submit], textarea").addClass("form-control");
+
+        //adding sections for scrollspy:
+        var h2s = $("h2");
+        console.log(h2s);
+
+        h2s.wrap(function(index){
+            return "<section id='section"  + index + "'></section>";
+        });
+
+        h2s.each(function(index){
+            links.append("a");
+        });
+
+    }
 )
